8ecfe4f91f8289c4675695910312f3acc208db12
dizsart <darogadibia@gmail.com>
2020-05-11 18:32:54 +0100

    added an approval card to the file structure

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
 
   return (
     <div className = "ui container comments">
+    <ApprovalCard>
     <CommentDetail author = {faker.name.firstName()} date = {faker.date.recent().toString()}/>
+    </ApprovalCard>
+    <ApprovalCard>
     <CommentDetail author = {faker.name.firstName()} date = {faker.date.recent().toString()}/>
+    </ApprovalCard>
+    <ApprovalCard>
     <CommentDetail author = {faker.name.firstName()} date = {faker.date.recent().toString()}/>
+    </ApprovalCard>
     </div>
 
   );
